5d8cdd0838ab2d214288295ae2c71f4fa7458226
Valeriia <valerieee.v.r@gmail.com>
2023-04-26 22:48:17 +0300

    First commit

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-let firstNumber;
-let secondNumber;
+// 1. Створити новий масив на 10 елементів з довільними числами. 
+// const newArray = [-10, 35, 20, 2, 0, 85, 77, 41, 45, 54];
+const newArray = [25, 35, 10, 17, 15, 3, 1, -45, -24, -11];
 
-firstNumber =prompt("Enter first number");
-secondNumber = prompt("Enter second number");
-
-const additionResult = Number(firstNumber)+Number(secondNumber);
-const subtractionResult = Number(firstNumber)-Number(secondNumber);
-const multiplicationResult = Number(firstNumber)*Number(secondNumber);
-const divisionResult = Number(firstNumber)/Number(secondNumber);
-
-if (firstNumber == '' || secondNumber == '') {
-    alert (`Error`)
-} else {
-    alert (`The result of addition ${additionResult}`);
-
-    if (Number(secondNumber) > Number(firstNumber)) {
-        confirm (`Are you sure you want to proceed with the operation?`) ? alert(`The result of subtraction ${subtractionResult}`) : null ;
-    } else {
-        alert(`The result of subtraction ${subtractionResult}`)
+// 2. Написати скрипт для пошуку суми елементів масиву
+let sum = 0;
+for (let i = 0; i < newArray.length; i++){
+    if(typeof newArray[i] == 'number'){  
+    sum = sum + newArray[i];
     }
-    alert(`The result of multiplication ${multiplicationResult}`);
-    if (secondNumber == 0) {
-        alert (`Division by zero is impossible`)
-            } else {
-        alert (`The result of division ${divisionResult}`)
-            }
-        } 
+}
+
+// 3. Написати скрипт, який буде шукати мінімальне та максимальне значення елементів масиву.
+// 4. В пунктах 2 та 3 потрібно робити перевірку на тип елементу масиву і не брати до уваги елементи які не є числами
+let minValue = Infinity;
+let maxValue = -Infinity;
+for (let i = 0; i < newArray.length; i++) {
+    if(typeof newArray[i] !== 'number'){
+        continue;
+    }else{
+        
+    if (maxValue < newArray[i]) {
+        maxValue = newArray[i];
+    }
+    if (minValue > newArray[i]) {
+        minValue = newArray[i];
+    }
+  } 
+}
+alert (`Сума всіх елементів: ${sum}`);
+alert (`Найбільше значення: ${maxValue}`);
+alert (`Найменше значення: ${minValue}`);
+    
+
+// 5. Написати скрипт який виведе наступне:
+// #
+// ##
+// ###
+// ####
+// #####
+let emptyString = "";
+for(let j = 0; j<5; j++){
+    emptyString += "#";
+    console.log(emptyString);
+}

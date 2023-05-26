d1405dfdf51cf4c49edd29abaa5f57eb3184a69d
Valeriia <valerieee.v.r@gmail.com>
2023-05-26 22:26:33 +0300

    First commit

--- script.js
+++ script.js
@@ -1,84 +1,63 @@
-// 1. Зробити функції для додавання, віднімання, множення і ділення чисел. В тілі функції обовʼязково добавити перевірку на те, чи є параметр числом
+/*Функція ділення,моження і т.п.*/
 
-let firstValue = prompt("Введіть перше число");
-let secondValue = prompt("Введіть друге число");
-if(firstValue == "" || secondValue == ""){
-    alert(`Error. Обрахунок неможливий`);
-}
-
-function getSum (first, second){
-    if(isNaN(first, second)){
-        alert(`First or second value is not a number!`);
-    }else{
-        let addResult = parseFloat(first) + parseFloat(second);
-        console.log(`Результат додавання: ${addResult}`);
+function sum(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+    return a + b;
     }
 }
-getSum(firstValue, secondValue);
 
-function getSub (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
-    }else{
-    let subResult = first - second;
-    console.log(`Результат віднімання: ${subResult}`);
+function difference(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+    return a - b;
     }
 }
-getSub(firstValue, secondValue);
 
-function getMult (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
-    }else{
-    let multipleResult = first * second;
-    console.log(`Результат множення: ${multipleResult}`);
+function product(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+    return a * b;
     }
 }
-getMult(firstValue, secondValue);
 
-function getDiv (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
-    }else{
-    let divResult = first / second;
-    if(secondValue == 0){
-        divResult = `Error. На нуль ділити не можна!`;
-    }
-    console.log(`Результат ділення: ${divResult}`);
+function quotient(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+    return a / b;
     }
 }
-getDiv(firstValue, secondValue);
 
+console.log(sum(10,2));
+console.log(difference(10,2));
+console.log(product(10,2));
+console.log(quotient(10,2));
 
-// 2. Зробити функції для пошуку мінімального і максимального числа в масиві. Масив має бути аргументом функції. Для пошуку мінімального та максимального чисел використовуйте різні цикли.
-let testArray = [4, "testText", 7, -1, 0, null, 90, 35, true, 15, -8];
+/*Фунція мінмах значення*/
 
-function minValueFunc(arr){
-let minValue = Infinity;
-for (let i = 0; i < arr.length; i++) {
-    if(typeof arr[i] !== 'number'){
-        continue;
-    }else{
-    if (minValue > arr[i]) {
-        minValue = arr[i];
-    }
-  } 
+function findMaxNumber (numbers) {
+    let maxNumber = numbers[0];
+    for (let i = 0; i < numbers.length; i++){
+        if (typeof numbers[i] == 'number') {
+            if (numbers[i] > maxNumber){
+                maxNumber = numbers [i];
+            }
+        }
+        };
+    return maxNumber;
 }
-console.log (`Найменше значення: ${minValue}`); 
-}
-minValueFunc(testArray);
 
-function maxValueFunc(arr){
-let maxValue = -Infinity;
-for (let j = 0; j < arr.length; j++) {
-    if(typeof arr[j] !== 'number'){
-        continue;
-    }else{
-    if (maxValue < arr[j]) {
-        maxValue = arr[j];
-    }
-  } 
+function findMinNumber (numbers) {
+    let minNumber = numbers[0];
+    for (let i = 0; i < numbers.length; i++){
+        if (typeof numbers[i] == 'number') {
+            if (numbers[i] < minNumber){
+                minNumber = numbers [i];
+            }
+        }
+        };
+    return minNumber;
 }
-console.log(`Найбільше значення: ${maxValue}`);
-}
-maxValueFunc(testArray);
+
+const numbers = [1, 4, 28, 472, 8652, 3, 84, 76, 2963, 9];
+
+const maxNumber = findMaxNumber (numbers);
+const minNumber = findMinNumber (numbers);
+
+console.log(maxNumber, minNumber);
